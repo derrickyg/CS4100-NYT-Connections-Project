ad60c1320d2957d079351138da4ca2f7ffd15d8c
Derrick Gan <gan.derrick@gmail.com>
2025-11-30 14:59:07 -0500

    fixed init and similarity

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 # connections_solver/__init__.py
 
 from .puzzle import Puzzle
-from similarity import SimilarityBackend, get_default_backend
+from .similarity import SimilarityBackend, get_default_backend
 
 __all__ = ["Puzzle", "SimilarityBackend", "get_default_backend"]
